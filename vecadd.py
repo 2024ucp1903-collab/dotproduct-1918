50d26df36cc6909f4c205ba53ae410e3010d1c97
Aman Choudhary <2023ucp1707@mnit.ac.in>
2025-11-17 15:26:38 +0530

    added vecadd.py (1)

---
+++ vecadd.py
@@ -0,0 +1,29 @@
+
+import numpy as np 
+import math
+
+  
+v = np.array([2, 1]) 
+s = np.array([3, -2]) 
+d = np.dot(v, s) 
+print("Output is: ",d) 
+
+
+from numba import jit, prange
+
+@jit(nopython=True, parallel=True)
+def parallel_dot(a, b):
+    result = np.zeros((len(a), len(b[0])), dtype=np.float64)
+    for i in prange(len(a)):
+        for j in range(len(b[0])):
+            for k in range(len(b)):
+                result[i, j] += a[i][k] * b[k][j]
+    return result
+
+a = np.array([[2, 1], [0, 3]])
+b = np.array([[1, 1], [3, 2]])
+result = parallel_dot(a, b)
+
+print(result)
+
+print("random change")
